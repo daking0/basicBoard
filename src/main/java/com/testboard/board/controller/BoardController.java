f415be86e57e661c543d5c801c663b47c8f64e60
angk2074 <angk2074@gmail.com>
2023-07-10 11:08:38 +0900

    8강 페이징 진행중 - 페이징객체

--- src/main/java/com/testboard/board/controller/BoardController.java
+++ src/main/java/com/testboard/board/controller/BoardController.java
@@ -70,7 +70,19 @@ public class BoardController {
     // /board/paging?page=1
     @GetMapping("/paging")
     public String paging(@PageableDefault(page = 1) Pageable pageable, Model model) {
-        pageable.getPageNumber();
-        Page<BoardDTO> boardDTOPage = boardService.paging(pageable);
+//        pageable.getPageNumber();
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        int blockLimit = 3;
+        int startPage = (((int)(Math.ceil((double)pageable.getPageNumber() / blockLimit))) - 1) * blockLimit + 1; // 1 4 7 10 ~~
+        int endPage = ((startPage + blockLimit - 1) < boardList.getTotalPages()) ? startPage + blockLimit - 1 : boardList.getTotalPages();
+
+        // page 갯수 20개
+        // 현재 사용자가 3페이지면 1 2 3
+        // 현재 사용자가 7페이지면 7 8 9
+        // 보여지는 페이지 갯수 3개
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "paging";
     }
 }
